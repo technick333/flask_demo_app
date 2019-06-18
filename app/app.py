55016809d1bf005265f982dcf6dca5efd9b3f09d
technick303 <technick303@gmail.com>
2019-06-18 17:20:46 +0200

    add docker file

--- app/app.py
+++ app/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index_page():
-    return "Hello world"
+    hostname = os.uname()[1]
+    return "<h1>This hostname {}</h1".format(hostname)
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0',debug = True)
